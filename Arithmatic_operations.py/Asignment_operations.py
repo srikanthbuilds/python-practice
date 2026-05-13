bb7f1a1544fed7aa21cafdb4f6e4121776d344a8
srikanthbuilds <parikibandasrikanthcse@gmail.com>
2026-05-13 06:57:14 +0530

    Add new Python files and update existing ones

---
+++ Arithmatic_operations.py/Asignment_operations.py
@@ -0,0 +1,30 @@
+# Addition using augmented operators
+a = 0
+a += 1
+print(a)
+
+# Subraction using augmented operators
+a -= 1
+print(a) 
+
+# Division using augmented operators
+
+b = 10
+b /= 3
+print(b)
+
+# Remainder using augmented operators
+c = 10
+c %= 3
+print(c)
+
+# Multiplication using augmented operators
+d = 10 
+d *= 2
+print(d)
+
+# Square using augmented operators
+e = 10
+e **= 2
+print(e)
+
